709aec9a7b203bc7bb7c58a5f26afa781473da21
AbdulrahmanSalah1 <abdoelrahmansalah0@gmail.com>
2024-09-18 19:16:08 +0300

    commit

--- eTickets/Data/ViewModels/ChangeRoleVM.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace eTickets.Data.ViewModels
-{
-    public class ChangeRoleVM
-    {
-        public string UserId { get; set; }
-        public string UserName { get; set; }
-        public string Role { get; set; }
-    }
-}
